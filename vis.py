73e596d0676d2edc4beb337574a9e66b61dc8dec
matlob <101573299+matlob@users.noreply.github.com>
2022-03-15 19:45:48 +0200

    Update vis.py

--- vis.py
+++ vis.py
@@ -2,6 +2,9 @@ import random
 import telebot
 from telebot import types
 ToKen="1894082224:AAEmXvFMPzFkHeFbsLe4Isq6VCTHg3vvATM" # Token bot
+A = types.InlineKeyboardMarkup(row_width=2)
+D =  types.InlineKeyboardButton(text = "- تشغيل البوت مجدداً ♻️",callback_data="relo")
+A.add(D)
 bot = telebot.TeleBot(ToKen)
 @bot.message_handler(commands = ["start"])
 def Start(message):
@@ -9,14 +12,14 @@ def Start(message):
  User = message.chat.username 
  ID = message.chat.id
  A = types.InlineKeyboardMarkup(row_width=2)
- B = types.InlineKeyboardButton(text ="✅  المبرمج" , url = "t.me/F_7_U")
+ B = types.InlineKeyboardButton(text ="✅  MATLOB" , url = "t.me/F_7_U")
  C = types.InlineKeyboardButton(text ="✅ قناة مبرمج  " , url = "https://t.me/PR2_NET")
- D =  types.InlineKeyboardButton(text = "✅ الحصول على كومبو  فيزات",callback_data="y")
+ D =  types.InlineKeyboardButton(text = "✅ الحصول على كومبو ",callback_data="y")
  A.add(B,C,D)
  bot.reply_to(message, """  
 *- اهلا عزيزي ( {} )                             
-- في بوت صنع فيزات
-- قم بــ ضغط على الحصول على كومبو فيزات               
+- في بوت صنع كومبو
+- قم بــ ضغط على الحصول على كومبو       
 - معرفك : [ @{} ]                                    
 - ايديك : [ {} ]                                        *
 """.format(Name,User,ID) , parse_mode = "markdown" , reply_markup = A)	
@@ -28,10 +31,13 @@ def answer(call):
         Iran(call.message)
     if call.data == "relo":
         Start(call.message)
-P  = types.InlineKeyboardButton(text = "الحصول على كومبو", callback_data= 'Iran')
+    if call.data =="da":
+        da(call.message)
+P  = types.InlineKeyboardButton(text = "الحصول على كومبو فيزات", callback_data= 'Iran')
+P1  = types.InlineKeyboardButton(text = "الحصول على كومبو ياهو", callback_data= 'da')
 def button(message):
     O0 = types.InlineKeyboardMarkup(row_width=1)
-    O0.add(P)
+    O0.add(P,P1)
     bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text="**Choose**",parse_mode='markdown',reply_markup=O0)          
 def Iran(message):
 	abc='1234567890'
@@ -46,8 +52,18 @@ def Iran(message):
 		xn = ('5'+uss+'|'+'0'+us+'|'+'202'+uk+'|'+am)
 		savefile.write(xn + "\n")
 	file = open('VisaX.txt','rb')
-	A = types.InlineKeyboardMarkup(row_width=2)
-	D =  types.InlineKeyboardButton(text = "- تشغيل البوت مجدداً ♻️",callback_data="relo")
-	A.add(D)
-	bot.send_document(message.chat.id,file,caption="<strong>New Combo</strong>",parse_mode="html", reply_markup = A)
+	bot.send_document(message.chat.id,file,caption="<strong>New Combo VISAT</strong>",parse_mode="html", reply_markup = A)
+	
+def da(message):
+		pro = 'qwertyuiopasdfghjklzxcvbnm'
+		wh = '0123456789'
+		B=open('yahoo.txt','w')
+		for x in range(1000):
+			h = str("".join(random.choice(wh)for i in range(3)))
+			ite = ("".join(random.choice(pro)for i in range(4)))
+			w = (f'{ite}{h}@yahoo.com:{ite}{h}')
+			B.write(w+'\n')
+		B.close()
+		file = open('yahoo.txt','rb')
+		bot.send_document(message.chat.id,file,caption="<strong>New Combo YAHOO</strong>",parse_mode="html", reply_markup = A)
 bot.polling()
